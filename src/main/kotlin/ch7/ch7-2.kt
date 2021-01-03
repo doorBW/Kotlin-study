ba1dbbf6419f8bb1ba046969fea6a5c579beb052
doorBW <doorbw@outlook.com>
2021-01-03 21:42:17 +0900

    ch7-2 code

---
+++ src/main/kotlin/ch7/ch7-2.kt
@@ -0,0 +1,16 @@
+package ch7
+
+class Person(
+    val firstname: String, val lastName: String
+): Comparable<Person>{
+    override fun compareTo(other: Person): Int {
+        println("# Person compare")
+        return compareValuesBy(this, other, Person::lastName, Person::firstname)
+    }
+}
+
+fun main(){
+    val p1 = Person("Alice", "Smith")
+    val p2 = Person("Bob", "Johnson")
+    println(p1 < p2)
+}
